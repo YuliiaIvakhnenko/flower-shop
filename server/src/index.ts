ea41bc183b21ad08caa6301d958d096015cee005
IvakhnenkoYuliia <ivakhnenko.yuliia@lll.kpi.ua>
2025-09-15 19:11:09 +0300

    Оновив код: додав деплой через Render

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,8 @@
-import express from "express";
+import express from "express"; 
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 import shopRoutes from "./routes/shops";
 import flowerRoutes from "./routes/flowers";
@@ -18,10 +19,20 @@ const DB_NAME = process.env.DB_NAME || "flowerShop";
 app.use(cors());
 app.use(express.json());
 
+
 app.use("/api/shops", shopRoutes);
 app.use("/api/flowers", flowerRoutes);
 app.use("/api/bouquets", bouquetRoutes);
 app.use("/api/orders", orderRoutes);
+
+
+const clientBuildPath = path.join(__dirname, "../client/build");
+app.use(express.static(clientBuildPath));
+
+
+app.get("*", (_req, res) => {
+  res.sendFile(path.join(clientBuildPath, "index.html"));
+});
 
 mongoose
   .connect(DB_URI, { dbName: DB_NAME })
