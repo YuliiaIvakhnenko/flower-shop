2b596ef81f13299e36d1a9510277f3b6a384580c
IvakhnenkoYuliia <ivakhnenko.yuliia@lll.kpi.ua>
2025-09-15 21:53:54 +0300

    Fix: install Node/Express types on Render; tsconfig types:node

--- server/src/index.ts
+++ server/src/index.ts
@@ -19,7 +19,6 @@ const DB_NAME = process.env.DB_NAME || "flowerShop";
 app.use(cors());
 app.use(express.json());
 
-// API
 app.use("/api/shops", shopRoutes);
 app.use("/api/flowers", flowerRoutes);
 app.use("/api/bouquets", bouquetRoutes);
@@ -27,11 +26,10 @@ app.use("/api/orders", orderRoutes);
 
 const clientBuildPath = path.resolve(__dirname, "../../client/build");
 
+app.use(express.static(clientBuildPath, { index: false }));
 
-app.use(express.static(clientBuildPath));
 
-
-app.get("'/(.*)", (_req, res) => {
+app.get(/^(?!\/api\/).*/, (_req, res) => {
   res.sendFile(path.join(clientBuildPath, "index.html"));
 });
 
@@ -39,14 +37,3 @@ app.use((req, res) => {
   if (!res.headersSent) res.status(404).send("Not Found");
 });
 
-mongoose
-  .connect(DB_URI, { dbName: DB_NAME })
-  .then(() => {
-    console.log("Connected to MongoDB");
-    app.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.error("MongoDB connection error:", err);
-  });
